cf4c7073c86d88878aade4d71f05a7553ad188a2
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-28 23:34:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,10 +14,10 @@ class Application
     elsif req.path.match(/cart/)
       if @@cart.length = 0 
         resp.write "Your cart is empty"
-      end 
       else 
         @@cart.each do |item|
           resp.write "#{item}\n"
+        end 
       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
